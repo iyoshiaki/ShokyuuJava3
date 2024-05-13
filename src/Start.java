a4633943d5d3faa9ba75973f81cfd6eac35683a6
yoshiaki <zeal23310053@fsgcl.jp>
2024-05-13 11:55:43 +0900

    2章練習問題を追加

---
+++ src/Start.java
@@ -0,0 +1,17 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 3;
+	    System.out.println(x*y);	
+		
+	    MyFrame frame1=new RectFrame();
+		frame1.setLocation(400,300);
+		
+		MyFrame frame3=new RectFrame();
+		frame3.setLocation(50,300);
+		
+	}
+
+}
