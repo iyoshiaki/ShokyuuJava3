866b1d4a94bab7d6bb57fb1e2ade719c75fee45f
yoshiaki <zeal23310053@fsgcl.jp>
2024-05-14 12:10:59 +0900

    3.3問題を追加

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -3,10 +3,20 @@ public class RectTest extends MyFrame
 {
 	public void run() 
 	{
+		setColor(255,255,0);	
 		fillRect(30,40,50,100);
-		fillRect(130,40,50,100);
-		fillRect(30,200,150,10);
 		
-		fillRect(90,160,30,30);	
+		setColor(200,0,255);
+		fillRect(130,40,50,100);
+		
+		setColor(128,200,255);
+		fillRect(230,40,50,100);
+		
+		setColor(0,0,0);
+		fillRect(330,40,50,100);
+
+		setColor(128,128,128);
+		fillRect(30,150,350,100);
+
 	}
 }
