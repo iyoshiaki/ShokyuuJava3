24d45affd3e285db3c7d89c56de9280ea9b9bb2e
yoshiaki <zeal23310053@fsgcl.jp>
2024-05-21 11:06:36 +0900

    追加課題2

--- 初級Java2.5/src/kadai.java
+++ 初級Java2.5/src/kadai.java
@@ -2,17 +2,21 @@
 public class kadai extends MyFrame
 {
 	public void run()
-	{	
+	{
 		setColor(0,128,0);
-		int x = 30;
-		int y = 20;
-		int a = 30;
-		while(y<400)
+	
+		for(int i=0,x = 30,y = 80,h = 10,n = 120;i<10;i++)
 		{
-			fillRect(x,y,a,a);
-			x = x+a;
-			y = y+a;
-			a+=10;
+			 fillRect(x,y,h,n);
+			 x+=20;
+			 n-=10;
+		}
+		for(int i=0,x = 30,y = 250,h = 10,n = 20;i<10;i++)
+		{
+			 fillRect(x,y,h,n);
+			 x+=20;
+			 y-=10;
+			 n+=10;
 		}
 	}
 
