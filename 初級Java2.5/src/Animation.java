a2f5243351554bea124e930560f63e44025fe300
yoshiaki <zeal23310053@fsgcl.jp>
2024-05-21 12:17:58 +0900

    5.2問題を追加

---
+++ 初級Java2.5/src/Animation.java
@@ -0,0 +1,18 @@
+
+public class Animation extends MyFrame
+{
+	public void run()
+	{
+		
+		int x = 30;
+		while(x<=170)
+		{
+			clear();
+			
+			setColor(0,128,0);
+			fillRect(x,80,10,100);
+			x+=1;
+			sleep(0.016);
+		}
+	}
+}
