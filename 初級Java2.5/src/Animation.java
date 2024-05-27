44d9d30d55177a896ce9e65cbc0b3e2812c68994
yoshiaki <zeal23310053@fsgcl.jp>
2024-05-27 10:47:08 +0900

    5.3問題を追加

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -4,15 +4,29 @@ public class Animation extends MyFrame
 	public void run()
 	{
 		
+		int y = 100;
 		int x = 30;
+		while(true) {
 		while(x<=170)
 		{
 			clear();
 			
 			setColor(0,128,0);
-			fillRect(x,80,10,100);
-			x+=1;
-			sleep(0.016);
+			fillRect(x,y,10,100);
+			
+			x+=5;
+			sleep(0.1);
+		}
+		while(x>=30)
+		{
+			clear();
+			
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			
+			x-=5;
+			sleep(0.1);
+		}
 		}
 	}
 }
