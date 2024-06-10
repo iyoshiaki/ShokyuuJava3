d17f41fc9a7c3135142fe52bf8c6ec21c25dc456
yoshiaki <zeal23310053@fsgcl.jp>
2024-06-10 10:43:29 +0900

    8.3問題を追加

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -4,7 +4,7 @@ public class Start {
 	public static void main(String[] args) 
 	{
 		System.out.println("プログラムを実行します");
-		MyFrame f=new DisplayCarTrain();
+		MyFrame frame1=new DisplayCarTrain();
 	}
 
 }
