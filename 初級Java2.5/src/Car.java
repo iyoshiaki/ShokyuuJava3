4bd1f5bc428fc5c59ba78158d319186aad174c3c
yoshiaki <zeal23310053@fsgcl.jp>
2024-06-11 09:51:03 +0900

    9.2問題を追加

--- 初級Java2.5/src/Car.java
+++ 初級Java2.5/src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle {
+public class Car extends Vehicle implements KeyListener {
 	public Car(int x, int y, int vx, int vy) {
 		super(x, y, vx, vy);
 	}
@@ -9,5 +11,28 @@ public class Car extends Vehicle {
 		frame.fillRect(x, y + 20, 80, 20);
 		frame.fillOval(x + 10, y + 40, 20, 20);
 		frame.fillOval(x + 50, y + 40, 20, 20);
+	}
+
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	public void keyPressed(KeyEvent e) {
+
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = -5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 5;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 0;
+		}
 	}
 }
